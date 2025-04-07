0e64872a29bd0e7da6fd8b5e67c82ee45cbff66e
teddyvn2017 <ware4u@gmail.com>
2025-04-07 17:25:51 +0700

    starting add framer motion to header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+'use client'
+
+import React from 'react';
+import { motion } from 'framer-motion';
 
 const Header = () => {
   return (
@@ -6,10 +9,22 @@ const Header = () => {
         <nav>
             <ul className='flex flex-row font-semibold mx-auto px-16 py-8 gap-8 text-base'>
                 <li>
-                    <a href="#" className='active px-[12px] py-1'>Home</a>
+                    <a href="#" className='active px-[12px] py-1 overflow-hidden'>Home</a>
                 </li>
                 <li>
-                    <a href="#">Products</a>
+                    <motion.div
+                        
+                        className="px-[12px] py-1 relative overflow-hidden inline-block"
+                        whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
+                        >
+                        Products
+                        <motion.div
+                            className="absolute inset-0 border-2 border-black rounded-md origin-top-left"
+                            initial={{ scaleX: 0, scaleY: 0 }}
+                            animate={{ scaleX: 1, scaleY: 1 }}
+                            transition={{ duration: 0.3 }}
+                        />
+                    </motion.div>
                 </li>
                 <li>
                     <a href="#">Categories</a>
